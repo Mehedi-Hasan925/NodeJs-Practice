ccfaf2476ec5ff08d482394bd2875451a79e7412
Mehedi-Hasan925 <mehedi.cse14bu@gmail.com>
2021-08-10 10:56:57 +0600

    handle a simple input in request in server

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 // core modules(fs,path,http,https,os)
 
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-    // console.log(req.method, req.url, req.headers);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>First nodejs response</title></head>');
-    res.write('<body><h1>This is my first nodejs server request</h1></body>');
-    res.write('</html>');
-    res.end();
-    process.exit();
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><input type="submit" value="send"></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+
+        req.on('end',()=>{
+            const bodyParse = Buffer.concat(body).toString();
+            // console.log(bodyParse);
+            const message = bodyParse.split('=')[1];
+            fs.writeFileSync('input.txt',message);
+        })
+
+        
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
 });
 
 server.listen(3001);

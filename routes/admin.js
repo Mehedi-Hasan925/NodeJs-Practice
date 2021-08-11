2c6f11d5850a3a0d4f3577ffa3c7becb20b6b28f
Mehedi-Hasan925 <mehedi.cse14bu@gmail.com>
2021-08-11 16:43:19 +0600

    setup views and static files

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,11 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 
+const rootDir = require('../util/rootDir')
+
 router.get("/add-product",(req,res,next)=>{
-    res.send('<form action="/add-product" method="POST"><input type="text" name="add"><input type="submit" value="add product"></form>');
+    res.sendFile(path.join(__dirname, '../','views','add_product.html'));
 });
 
 router.post("/add-product",(req,res,next)=>{
